c8cead594abb7c89129a30899899f04a2935fed2
suna <sn.sable005@gmail.com>
2020-08-23 00:49:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -74,6 +74,9 @@ def get_replytext(text):
         reply_text = 'これを聴いてるニャン\n' + music_lines[random.randint(0,(len(music_lines) - 1))].strip()
     elif ('てんき' in text) or ('きおん' in text) or ('天気' in text) or ('気温' in text) or ('降水' in text):
         reply_text = 'ここを見てるニャン\n' + 'https://www.google.co.jp/search?q=天気'
+    elif ('おやすみ' in text):
+        #reply_text = 'おやすみなさいニャン'
+        reply_text = 52002764
     else:
         # get reply messgage(A3RT/TalkAPI)
         reply_text = talkapi_response(text) + 'ニャン'
