49ff21e20764ac2b9952f8b0ca5ae401d470da01
suna <sn.sable005@gmail.com>
2020-08-23 23:51:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -100,7 +100,8 @@ def get_replymessage(text):
     elif ('映画' in text):
         popular_all = ''
         for p in popular:
-            popular_all += '\n\n' + '📽' + p.title + '\n' + p.overview
+            #popular_all += '\n\n' + '📽' + p.title + '\n' + p.overview
+            popular_all += '\n\n' + '📽' + p.title
         reply_text = '映画のトレンドだニャン' + popular_all
         reply_type = 'text'
     elif ('てんき' in text) or ('きおん' in text) or ('天気' in text) or ('気温' in text) or ('降水' in text):
