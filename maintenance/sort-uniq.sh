1b77904b4dc4a29cf4dc0421266dbdee40c6124f
suna <sn.sable005@gmail.com>
2020-08-25 16:26:10 +0900

    fix Usage

--- maintenance/sort-uniq.sh
+++ maintenance/sort-uniq.sh
@@ -4,7 +4,7 @@ set -eu
 
 if [ $# -ne 1 ]; then
   echo '# Removes duplicate lines after sorting the input file.'
-  echo 'Usage: ./'${0}' <inputfile_path>'
+  echo 'Usage: '${0}' <inputfile_path>'
   exit 1
 fi
 
