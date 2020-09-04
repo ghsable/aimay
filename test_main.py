c807b2199fe615a2a60110e9557215d8fb2ce4a7
suna <sn.sable005@gmail.com>
2020-09-04 19:37:16 +0900

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -10,7 +10,10 @@ import main
 class TestMain(unittest.TestCase):
  
   def test_f(self):
-    self.assertEqual(3, main.f()) 
+    self.assertEqual(3, main.f())
+  
+  def test_callback(self):
+    self.assertEqual('OK', main.callback())
 
 if __name__ == "__main__":
     unittest.main()
