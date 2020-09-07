d8851b29b49b9e8c756a909c44a30db37f369eb6
suna <sn.sable005@gmail.com>
2020-09-07 16:50:50 +0900

    Update __main__.py

--- aimay/__main__.py
+++ aimay/__main__.py
@@ -123,7 +123,8 @@ def get_replymessage(push_text):
 
 # return reply message(from data/*.txt)
 def return_data(filename):
-    filepath = os.getcwd() + '/aimay/data/' + filename
+    #filepath = os.getcwd() + '/aimay/data/' + filename
+    filepath = os.path.join(os.getcwd(), "aimay", "data", filename)
     with open(filepath) as datafile:
         datalines = datafile.readlines()
     return datalines[random.randint(0,(len(datalines) - 1))].strip()
