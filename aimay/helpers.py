e6907662c80405beca81382eeb2d8ed65892d554
suna <sn.sable005@gmail.com>
2020-09-07 17:16:44 +0900

    Update helpers.py

--- aimay/helpers.py
+++ aimay/helpers.py
@@ -1,1 +1,10 @@
+import os
+import random
+
+# return reply message(from data/*.txt)
+def return_data(filename):
+    filepath = os.path.join(os.getcwd(), 'aimay/data', filename)
+    with open(filepath) as datafile:
+        datalines = datafile.readlines()
+    return datalines[random.randint(0,(len(datalines) - 1))].strip()
 
