2e86bf689bc2ee534b5d047c9a4f16f3fcb53e8e
suna <sn.sable005@gmail.com>
2020-09-07 15:22:11 +0900

    Update helpers.py

--- aimay/helpers.py
+++ aimay/helpers.py
@@ -1,12 +1,1 @@
-import os      # Heroku
-import random  # GitHub
-import pya3rt  # A3RT/TalkAPI:requirements.txt
-
-# return reply message(from data/*.txt)
-def return_data(filename):
-    filepath = os.getcwd() + '/data/' + filename
-    with open(filepath) as datafile:
-        datalines = datafile.readlines()
-    return datalines[random.randint(0,(len(datalines) - 1))].strip()
-
 
