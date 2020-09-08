b586172b3015493ed4a0ede709530391b6046f50
ghsable <sn.sable005@gmail.com>
2020-09-08 22:02:33 +0900

    Update helpers.py

--- aimay/helpers.py
+++ aimay/helpers.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 
+# return reply message(from data/*.txt)
 import os
 import random
-import pya3rt
-
-A3RT_TALKAPI_APIKEY = os.environ["A3RT_TALKAPI_APIKEY"] # A3RT/TalkAPI
-
-# return reply message(from data/*.txt)
 def return_data(filename):
     filepath = os.path.join(os.getcwd(), 'aimay/data', filename)
     with open(filepath) as datafile:
@@ -14,7 +10,9 @@ def return_data(filename):
     return datalines[random.randint(0,(len(datalines) - 1))].strip()
 
 # return reply message(A3RT/TalkAPI)
+import pya3rt
 def talkapi_response(push_text):
-    talkapi_client   = pya3rt.TalkClient(A3RT_TALKAPI_APIKEY)
+    talkapi_client   = pya3rt.TalkClient(os.environ.get('A3RT_TALKAPI_APIKEY'))
     talkapi_response = talkapi_client.talk(push_text)
     return ((talkapi_response['results'])[0])['reply']
+
