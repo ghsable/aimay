daf52d057db0f0bb12f89fba2a913910712308fb
ghsable <sn.sable005@gmail.com>
2020-09-08 22:14:01 +0900

    Update __main__.py

--- aimay/__main__.py
+++ aimay/__main__.py
@@ -60,7 +60,7 @@ def handle_message(event):
     else:
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text='エラーみたいだニャン'))
+            TextSendMessage(text='error'))
 
 if __name__ == "__main__":
    # get port from Heroku
