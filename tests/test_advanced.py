08724557584b7f2383ab1a6aff24647721980a48
suna <sn.sable005@gmail.com>
2021-08-20 14:54:05 +0900

    init

--- tests/test_advanced.py
+++ tests/test_advanced.py
@@ -38,11 +38,11 @@ class TestCore(unittest.TestCase):
     self.assertIsNone(reply[2])
     self.assertIsNone(reply[3])
 
-#    reply = aimay.get_reply('text', '今日のニュースを知りたいな')
-#    self.assertEqual('text', reply[0])
-#    self.assertIsNotNone(reply[1])
-#    self.assertIsNone(reply[2])
-#    self.assertIsNone(reply[3])
+    reply = aimay.get_reply('text', '今日のニュースを知りたいな')
+    self.assertEqual('text', reply[0])
+    self.assertIsNotNone(reply[1])
+    self.assertIsNone(reply[2])
+    self.assertIsNone(reply[3])
 
     reply = aimay.get_reply('text', '面白いドラマあるかな')
     self.assertEqual('text', reply[0])
